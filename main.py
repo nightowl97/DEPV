41c11c005071e42254751b2a4171c51d6bd43c06
Youssef Khar <feverier@hotmail.fr>
2020-03-11 17:53:46 +0100

    object oriented approach

--- main.py
+++ main.py
@@ -1,213 +1,62 @@
-import csv
-import matplotlib.pyplot as plt
-import scipy.constants as sc
-import numpy as np
-import pvlib
-import decimal
-from celluloid import Camera
+from objects import *
 
+# RTC FRANCE #######################
+# b = {'rp':  [2, 100],
+#      'rs':  [0, 1],
+#      'a':   [1, 2],
+#      'i0':  [1e-07, 1e-04],
+#      'ipv': [0, 10]
+#      }
+#
+# algo = DE(b, [*read_csv("data/RTC33D1000W.csv")], 1, 1)
+# T = 33 + 275.15
+# Vt = sc.Boltzmann * T / sc.elementary_charge  # Thermal voltage
+# algo.solve(Vt)
+# algo.plot_fit_hist()
+# algo.plot_result(Vt)
 
-#%% CONTROL PARAMETERS & CONSTANTS
-temp_c = 55  # Temperature in celsius
-T = temp_c + 273.15  # Temperature
-Ns = 36  # Number of cells in series
-Np = 1  # Number of cells in parallel
+# Schutten solar STP 6 #########################################
+# b = {'rp':  [2, 300],
+#      'rs':  [0, 1],
+#      'a':   [1, 2],
+#      'i0':  [1e-07, 1e-5],
+#      'ipv': [0, 10]
+#      }
+#
+# algo = DE(b, [*read_csv("data/STP6")], 36, 1)
+# T = 55 + 275.15
+# Vt = sc.Boltzmann * T / sc.elementary_charge  # Thermal voltage
+# algo.solve(Vt)
+# algo.plot_fit_hist()
+# algo.plot_result(Vt)
+
+# STM 6 Single Diode #######################
+b = {'rp':  [2, 300],
+     'rs':  [0, 1],
+     'a':   [1, 2],
+     'i0':  [1e-07, 1e-5],
+     'ipv': [0, 10]
+     }
+
+algo = DE(b, [*read_csv("data/STM6_4036")], 36, 1)
+T = 51 + 275.15
 Vt = sc.Boltzmann * T / sc.elementary_charge  # Thermal voltage
-GENMAX = 100  # Max number of generations
-NP = 100  # Population size
-F = 0.7  # Mutation factor (Values > 0.4 are recommended [1])
-CR = 0.8  # Crossover rate (High values recommended [2])
-D = 2  # 2-Dimensional search space (a, Rs)
-# Search space according to literature ([3] Oliva et al., 2017 and [4] Yu et al. 2017)
-a_L, a_H = 1, 2  # Ideality factor boundaries
-Rs_L, Rs_H = 0, 0.5  # Series resistance Rs
-fit_hist = []  # Store fitness history
-avg_fit_hist = []
+algo.solve(Vt)
+algo.plot_fit_hist()
+algo.plot_result(Vt)
 
-#%% DATA COLLECTION
-# CSV file
-with open("data/STP6") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    next(csv_reader)  # Ignore the header
-    data = [(float(row[0]), float(row[1])) for row in csv_reader]
-    voltages, currents = np.array([row[0] for row in data]), np.array([row[1] for row in data])
-    N = len(voltages)
-    xlim = max(voltages) + int(.1 * max(voltages))
-    ylim = max(currents) + int(.1 * max(currents)) + .02
-
-
-#%% PIVOT POINT SELECTION
-p1, p2 = data[0], data[-1]  # Isc and Voc
-# Maximum power point (MPP)
-powers = [a * b for a, b in zip(voltages, currents)]
-p3 = data[powers.index(max(powers))]
-p = -2  # First MPP
-p3_fit = []  # store fittest of each p3 run
-
-
-def get_5_from_2(a, rs, mpp):
-    # Finds the 5 single diode model params from a, rs, p1, p2 and p3
-    # Re-extract the 5 parameters from each solution vector by formfitting on the pivot points
-
-    v3, v2, v1 = mpp[0], p2[0], p1[0]
-    i3, i2, i1 = mpp[1], p2[1], p1[1]
-    t = temp_c + 273.15  # Temperature
-    loc_vt = (Ns * sc.Boltzmann * t) / sc.elementary_charge
-
-    alpha = v3 - v1 + (rs * (i3 - i1) * (Ns / Np))
-    beta = v2 - v1 + (rs * (i2 - i1) * (Ns / Np))
-
-    i0 = (alpha * (i2 - i1) + beta * (i3 - i1)) / (Np * (alpha * (
-            np.exp((v1 + i1 * rs * (Ns / Np)) / (a * loc_vt)) -
-            np.exp((v2 + i2 * rs * (Ns / Np)) / (a * loc_vt))) + beta * (
-            np.exp((v1 + i1 * rs * (Ns / Np)) / (a * loc_vt)) -
-            np.exp((v3 + i3 * rs * (Ns / Np)) / (a * loc_vt)))))
-
-    rp = ((v1 - v2) * (Np / Ns) + rs * (i1 - i2)) / (i2 - i1 - i0 * Np * (
-            np.exp((v1 + i1 * rs * (Ns / Np)) / (a * loc_vt)) -
-            np.exp((v2 + i2 * rs * (Ns / Np)) / (a * loc_vt))))
-
-    ipv = (i0 * Np * np.expm1((v1 + i1 * rs * (Ns / Np)) / (a * loc_vt)) +
-           ((v1 + i1 * rs * (Ns / Np)) / (rp * (Ns / Np))) + i1) * (1 / Np)
-
-    return rp, rs, a, i0, ipv
-
-
-# Evaluation function using the three pivot points
-def evaluate(a, Rs, p3):
-    # J function calculation (RMSE)
-    # Find the current using the Lambert W function [5]
-    Rp, Rs, a, I0, Ipv = get_5_from_2(a, Rs, p3)
-    Ical = pvlib.pvsystem.i_from_v(Rp * Ns / Np, Rs * Ns / Np, a * Ns * Vt, voltages, I0 * Np, Ipv * Np)
-    J = currents - Ical
-    # Fitness penalty (J = 100) for unphysical values of Ipv, I0 and Rp
-    if I0 < 0 or 1000 < Rp or Rp < 0 or Ipv < 0:
-        return 100
-
-    return np.sqrt(np.mean(J ** 2))  # RMSE
-
-
-def plot_from_2(a, rseries, mpp):
-    # Plots I-V curve from the two parameters a, Rs
-    plt.plot(voltages, currents, 'go')
-    v = np.linspace(0, xlim, 100)
-    rp, rs, a, i0, ipv = get_5_from_2(a, rseries, mpp)
-    ical = pvlib.pvsystem.i_from_v(rp * Ns / Np, rs * Ns / Np, a * Ns * Vt, v, i0 * Np, ipv * Np)
-    plt.plot(v, ical)
-    plt.xlabel("Voltage V(V)")
-    plt.ylabel("Current I(A)")
-    plt.grid()
-    plt.axis([0, xlim, 0, ylim])
-    plt.show()
-    plt.figure()
-
-
-# fig = plt.figure()
-# camera = Camera(fig)
-while p <= 2:
-
-    gen = 1
-    p3 = data[powers.index(max(powers)) + p * int(.1 * N)]  # P(-1), P(0) and P(1)
-    POP = np.random.uniform([a_L, Rs_L], [a_H, Rs_H], size=(NP, 2))  # Population initialization
-
-    while gen <= GENMAX:
-
-        fitness = np.asarray([evaluate(vec[0], vec[1], p3) for vec in POP])
-        fittest_index = np.argmin(fitness)
-        fittest = POP[fittest_index]
-        fit_hist.append(fitness[fittest_index])
-        avg_fit_hist.append(np.average(fitness))
-
-        for i in range(NP):
-            a, Rs = POP[i]
-            # Generate donor vector from 2 random vectors and the fittest
-            indices = [ind for ind in range(NP) if ind != i and ind != fittest_index]
-            i1, i2 = np.random.choice(indices, 2, replace=False)
-            # DE mutation
-            mutant = fittest + F * (POP[i1] - POP[i2])
-            # Crossover
-            crosspoints = np.random.rand(D) <= CR
-            if not any(crosspoints):
-                trial = np.where(np.random.randint(0, D), mutant, POP[i])
-            else:
-                trial = np.where(crosspoints, mutant, POP[i])
-            # Penalty
-            if not a_L <= trial[0]:
-                trial[0] = a_L + np.random.rand() * (a_H - a_L)
-            if not a_H >= trial[0]:
-                trial[0] = a_H - np.random.rand() * (a_H - a_L)
-            if not Rs_L <= trial[1]:
-                trial[1] = Rs_L + np.random.rand() * (Rs_H - Rs_L)
-            if not Rs_H >= trial[1]:
-                trial[1] = Rs_H - np.random.rand() * (Rs_H - Rs_L)
-
-            # Selection
-            f = evaluate(trial[0], trial[1], p3)
-            if f < fitness[i]:
-                fitness[i] = f
-                POP[i] = trial  # Switch
-                # Movie plotting
-                # plt.plot(voltages, currents, 'go')
-                # v = np.linspace(0, xlim, 100)
-                # rp, rs, a, i0, ipv = get_5_from_2(a, Rs, p3)
-                # ical = pvlib.pvsystem.i_from_v(rp * Ns / Np, rs * Ns / Np, a * Ns * Vt, v, i0 * Np, ipv * Np)
-                # plt.plot(v, ical)
-                # plt.xlabel("Voltage V(V)")
-                # plt.ylabel("Current I(A)")
-                # plt.grid()
-                # plt.axis([0, xlim, 0, ylim])
-                # camera.snap()
-
-                if f < fitness[fittest_index]:
-                    fittest_index = i
-                    fittest = trial
-
-        gen += 1
-    # animation = camera.animate()
-    # animation.save("evol{}-{}.mp4".format(p, gen))
-
-    plot_from_2(*fittest, p3)
-    p3_fit.append((*fittest, p3))
-    p += 1
-
-fittest = p3_fit[np.argmin([evaluate(*e) for e in p3_fit])]
-
-
-print("MPP: {}".format(fittest))
-print("RESULTS:\n(Rp:{}, Rs:{}, a:{}, I0:{}, Ipv:{})".format(*get_5_from_2(*fittest)))
-print("RMSE: {:.2E}".format(decimal.Decimal(evaluate(*fittest))))
-# print(get_5_from_2(1.5656, 0.4186e-3, (16.4, 1.542)))
-# print(evaluate(1.5656, 0.4186e-3, (16.4, 1.542)))
-
-# DATA REPRESENTATION
-plot_from_2(*fittest)
-
-plt.plot(avg_fit_hist, 'b')
-plt.grid()
-plt.xlabel("Generation")
-plt.ylabel("RMSE")
-plt.xlim((0, GENMAX - 1))
-plt.show()
-exit(0)
-
-"""
-References
-[1] Price K, Storn RM, Lampinen JA. Differential evolution: a practical
-approach to global optimization. Springer Science & Business Media; 2006 Mar 4.
-[2]  Wei H, Cong J, Lingyun X, Deyun S. Extracting solar cell model parameters 
-based on chaos particle swarm algorithm. In2011 International Conference on 
-Electric Information and Control Engineering 2011 Apr 15 (pp. 398-402). IEEE.
-URL: https://ieeexplore.ieee.org/abstract/document/5777246/
-[3] Yu K, Chen X, Wang X, Wang Z. Parameters identification of photovoltaic 
-models using self-adaptive teaching-learning-based optimization. Energy 
-Conversion and Management. 2017 Aug 1;145:233-46.
-URL: https://www.sciencedirect.com/science/article/pii/S0306261917305330
-[4] Oliva D, El Aziz MA, Hassanien AE. Parameter estimation of photovoltaic 
-cells using an improved chaotic whale optimization algorithm. 
-Applied Energy. 2017 Aug 15;200:141-54.
-URL: https://www.sciencedirect.com/science/article/pii/S0196890417303655
-[5] A. Jain, A. Kapoor, "Exact analytical solutions of the
-parameters of real solar cells using Lambert W-function", Solar
-Energy Materials and Solar Cells, 81 (2004) 269-277.
-URL: https://www.sciencedirect.com/science/article/pii/S0927024803002605
-"""
+# STM6 Double Dide ##########################
+b = {'rp':  [2, 300],
+     'rs':  [0, 1],
+     'a1':   [1, 2],
+     'a2':   [1, 2],
+     'i01': [1e-7, 1e-5],
+     'i02': [1e-7, 1e-5],
+     'ipv': [0, 10]
+     }
+algo = DE(b, [*read_csv("data/STM6_4036")], 36, 1)
+T = 51 + 275.15
+Vt = sc.Boltzmann * T / sc.elementary_charge  # Thermal voltage
+algo.solve(Vt)
+algo.plot_fit_hist()
+algo.plot_result(Vt)
---
+++ objects.py
@@ -0,0 +1,250 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.special import lambertw
+import scipy.constants as sc
+
+
+# Reads experimental IV data from a csv file
+def read_csv(filename):
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader)  # Ignore the header
+        data = [(float(row[0]), float(row[1])) for row in csv_reader]
+        voltages, currents = np.array([row[0] for row in data]), np.array([row[1] for row in data])
+        return voltages, currents
+
+
+class DE:
+
+    def __init__(self, bounds, ivdata, Ns, Np, popsize=100, maxiter=200, mutf=0.7, crossr=0.8):
+        """
+        :param bounds: Dictionary of bounds in this form {'rp': [lower, upper], 'rs': [lower, upper] ...}
+        :param dim: dimensions of search space (single diode 5, double diode 7)
+        :param ivdata: [Voltages, Currents]
+        """
+        self.bounds = bounds  #
+        self.popsize = popsize
+        self.maxiter = maxiter
+        self.mutf = mutf
+        self.crossr = crossr
+        self.dim = len(bounds)  # Dimensions of the search space
+        self.populations = self.maxiter * [self.popsize * [None]]  # Initial populations
+        self.fitnesses = self.maxiter * [self.popsize * [100]]  # Initial fitnesses
+        self.ivdata = ivdata
+        self.Ns = Ns
+        self.Np = Np
+
+    # Main differential evolution algorithm
+    def solve(self, vth):
+        # Initial uniform distribution
+        self.populations[0] = np.random.uniform([i[0] for i in self.bounds.values()],
+                                                [j[1] for j in self.bounds.values()], size=(self.popsize, self.dim))
+        for gen in range(self.maxiter):
+            # Initial fitness
+            self.fitnesses[gen] = [DE.objf(vec, self.ivdata, vth, self.Ns, self.Np) for vec in self.populations[gen]]
+
+            fittest_index = np.argmin(self.fitnesses[gen])
+            fittest = self.populations[gen][fittest_index]
+
+            # Iterate over population
+            for i in range(self.popsize):
+                # Generate donor vector from 2 random vectors and the fittest
+                indices = [ind for ind in range(self.popsize) if ind != i and ind != fittest_index]
+                i1, i2 = np.random.choice(indices, 2, replace=False)
+
+                # DE mutation
+                mutant = fittest + self.mutf * (self.populations[gen][i1] - self.populations[gen][i2])
+
+                # Crossover
+                crosspoints = np.random.rand(self.dim) <= self.crossr
+                if not any(crosspoints):
+                    crosspoints[np.random.randint(0, self.dim)] = True
+                trial = np.where(crosspoints, mutant, self.populations[gen][i])
+
+                # Penalty
+                for j, key in enumerate(self.bounds.keys()):
+                    lower, upper = self.bounds[key]
+                    # Solution vectors need to be [rp, rs, a, i0, iph]
+                    if not lower <= trial[j]:
+                        trial[j] = lower + np.random.rand() * (upper - lower)
+                    if not upper >= trial[j]:
+                        trial[j] = upper - np.random.rand() * (upper - lower)
+
+                # Selection
+                f = self.objf(trial, self.ivdata, vth, self.Ns, self.Np)
+
+                if gen + 1 < self.maxiter:
+                    if f < self.fitnesses[gen][i]:
+                        #  Select for next generation
+                        self.populations[gen + 1][i] = trial
+                        self.fitnesses[gen + 1][i] = f
+                    else:
+                        self.populations[gen + 1][i] = self.populations[gen][i]
+
+    @staticmethod
+    def objf(vector, exp_data, vth, Ns, Np):
+        # If vector is none, max fitness
+        if vector is None:
+            return 100
+        voltages = exp_data[0]
+        ical = DE.i_from_v(vector, voltages, vth, Ns, Np)
+        delta = ical - exp_data[1]  # compare currents for each voltage value
+        # Fitness penalty (J = 100) for unphysical values of Rs and Rp
+        if vector[0] < 0 or vector[1] < 0:
+            return 100
+        return np.sqrt(np.mean(delta ** 2))
+
+    @staticmethod
+    def i_from_v(vector, v, vth, Ns, Np):
+        # Takes a solution vector of 5/7 params and a voltage to calculate the current
+        if vector is not None:
+            if len(vector) != 5 and len(vector) != 7:
+                print("V:{}".format(vector))
+                print("Vector has {} parameters! needs 5 or 7.\n Exiting..".format(len(vector)))
+                exit(0)
+
+        output_is_scalar = np.isscalar(v)
+
+        # For numerical stability: Gsh=1/Rsh, so Rsh=np.inf ==> Gsh=0
+        conductance_shunt = 1. / (vector[0] * (Ns / Np))
+        #  Single diode
+        if len(vector) == 5:
+            resistance_shunt, resistance_series, n, saturation_current, photocurrent = vector
+
+            Gsh, Rs, a, V, I0, IL = np.broadcast_arrays(conductance_shunt, resistance_series * (Ns / Np),
+                                                        n * Ns * vth, v, saturation_current * Np,
+                                                        photocurrent * Np)
+
+            # Initialize output I (V might not be float64)
+            current = np.full_like(V, np.nan, dtype=np.float64)  # noqa: E741, N806
+
+            # Determine indices where 0 < Rs requires implicit model solution
+            idx_p = 0. < Rs
+
+            # Determine indices where 0 = Rs allows explicit model solution
+            idx_z = 0. == Rs
+
+            # Explicit solutions where Rs=0
+            if np.any(idx_z):
+                current[idx_z] = IL[idx_z] - I0[idx_z] * np.expm1(V[idx_z] / a[idx_z]) - \
+                           Gsh[idx_z] * V[idx_z]
+
+            # possibility of overflow
+            if np.any(idx_p):
+                # LambertW argument
+                argW = Rs[idx_p] * I0[idx_p] / (a[idx_p] * (Rs[idx_p] * Gsh[idx_p] + 1.)) * \
+                       np.exp((Rs[idx_p] * (IL[idx_p] + I0[idx_p]) + V[idx_p]) /
+                              (a[idx_p] * (Rs[idx_p] * Gsh[idx_p] + 1.)))
+
+                # lambertw typically returns complex value with zero imaginary part
+                # may overflow to np.inf
+                lambertwterm = lambertw(argW).real
+
+                # Jain and Kapoor, 2004
+                current[idx_p] = (IL[idx_p] + I0[idx_p] - V[idx_p] * Gsh[idx_p]) / \
+                                 (Rs[idx_p] * Gsh[idx_p] + 1.) - (a[idx_p] / Rs[idx_p]) * lambertwterm
+
+            if output_is_scalar:
+                return current.item()
+            else:
+                return current
+
+        # Double diode
+        if len(vector) == 7:
+            resistance_shunt, resistance_series, n1, n2, saturation_current1, saturation_current2, photocurrent = vector
+            Gsh, Rs, a1, a2, V, I01, I02, IL = np.broadcast_arrays(conductance_shunt, resistance_series, n1 * Ns * vth,
+                                                        n2 * Ns * vth, v, saturation_current1 * Np,
+                                                        saturation_current2 * Np, photocurrent * Np)
+            # Intitalize output I (V might not be float64)
+            current = np.full_like(V, np.nan, dtype=np.float64)
+            # Determine indices where 0 < Rs requires implicit model solution
+            idx_p = 0. < Rs
+
+            # Determine indices where 0 = Rs allows explicit model solution
+            idx_z = 0. == Rs
+
+            # Explicit solutions where Rs=0
+            if np.any(idx_z):
+                current[idx_z] = IL[idx_z] - I01[idx_z] * np.expm1(V[idx_z] / a1[idx_z]) - \
+                                 I02[idx_z] * np.expm1(V[idx_z] / a2[idx_z]) - \
+                                 Gsh[idx_z] * V[idx_z]
+
+            # Only compute using LambertW if there are cases with Rs>0
+            # Does NOT handle possibility of overflow, github issue 298
+            if np.any(idx_p):
+                # LambertW arguments, cannot be float128, may overflow to np.inf
+                argw1 = (Rs[idx_p] * (I01[idx_p] + I02[idx_p]) / (a1[idx_p] * (Gsh[idx_p] * Rs[idx_p] + 1))) * \
+                        np.exp((Rs[idx_p] * (IL[idx_p] + I01[idx_p] + I02[idx_p]) + V[idx_p]) /
+                               (a1[idx_p] * (Gsh[idx_p] * Rs[idx_p] + 1))
+                )
+                argw2 = (Rs[idx_p] * (I01[idx_p] + I02[idx_p]) / (a2[idx_p] * (Gsh[idx_p] * Rs[idx_p] + 1))) * \
+                        np.exp((Rs[idx_p] * (IL[idx_p] + I01[idx_p] + I02[idx_p]) + V[idx_p]) /
+                               (a2[idx_p] * (Gsh[idx_p] * Rs[idx_p] + 1))
+                )
+                # lambertw typically returns complex value with zero imaginary part
+                # may overflow to np.inf
+                lambertwterm1 = lambertw(argw1).real
+                lambertwterm2 = lambertw(argw2).real
+
+                # Eqn. 2 in Jain and Kapoor, 2004
+                #  I = -V/(Rs + Rsh) - (a/Rs)*lambertwterm + Rsh*(IL + I0)/(Rs + Rsh)
+                # Recast in terms of Gsh=1/Rsh for better numerical stability.
+                # Eqn. 2 in Jain and Kapoor, 2004
+                #  I = -V/(Rs + Rsh) - (a/Rs)*lambertwterm + Rsh*(IL + I0)/(Rs + Rsh)
+                # Recast in terms of Gsh=1/Rsh for better numerical stability.
+                current[idx_p] = (IL[idx_p] + I01[idx_p] + I02[idx_p] - (V[idx_p] * Gsh[idx_p])) / \
+                                 (Gsh[idx_p] * Rs[idx_p] + 1) - (a1[idx_p] / (2 * Rs[idx_p])) * lambertwterm1 - \
+                                 (a2[idx_p] / (2 * Rs[idx_p])) * lambertwterm2
+
+            if output_is_scalar:
+                return current.item()
+            else:
+                return current
+
+    # PLOTTING
+    def plot_solution(self, vector, vth):
+        # Plot experimental points
+        voltages, currents = self.ivdata
+        xlim, ylim = max(voltages), max(currents)
+        plt.plot(voltages, currents, 'go')
+
+        # Calculate given solution adding 20% voltage axis length
+        v = np.linspace(0, xlim + (.2 * xlim), 100)
+        calc_current = DE.i_from_v(vector, v, vth, self.Ns, self.Np)
+
+        # Plotting (evil muuuhahahahaa)
+        plt.plot(v, calc_current)
+        plt.xlabel("Voltage V(V)")
+        plt.ylabel("Current I(A)")
+        plt.grid()
+        plt.axis([0, xlim + (.2 * xlim), 0, ylim + (.2 * ylim)])
+        plt.show()
+        plt.figure()
+
+    def plot_fit_hist(self):
+        assert self.maxiter == len(self.fitnesses)
+        averages = [np.average(generation) for generation in self.fitnesses]
+        plt.plot(averages, 'b')
+        plt.grid()
+        plt.xlabel("Generation")
+        plt.ylabel("RMSE")
+        plt.xlim((0, self.maxiter - 1))
+        plt.show()
+        plt.figure()
+
+    def plot_result(self, vth):
+        fittest_index = np.argmin(self.fitnesses[-1])
+        result = self.populations[-1][fittest_index]
+        result_fitness = self.fitnesses[-1][fittest_index]
+        assert result_fitness == np.min(self.fitnesses[-1][fittest_index])
+        self.plot_solution(result, vth)
+        if self.dim == 5:
+            print("Rsh = {}\nRs = {}\na = {}\nI0 = {}\nIpv = {}".format(*result))
+        elif self.dim == 7:
+            print("Rsh = {}\nRs = {}\na1 = {}\na2 = {}\nI01 = {}\nI02 = {}\nIpv = {}".format(*result))
+        else:
+            print("This should never execute")
+        print("Root Mean Squared Error:\t{}".format(result_fitness))
+
+
